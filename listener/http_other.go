9a56f677382cb706a7f3da69a4dbf210eb2137c4
Doridian <git@doridian.net>
2025-04-22 12:50:55 -0700

    Allow running with sudo if needed

--- listener/http_other.go
+++ listener/http_other.go
@@ -4,6 +4,7 @@ package listener
 
 import (
 	"net/http"
+	"os"
 	"os/exec"
 
 	"github.com/FoxDenHome/shutdownd/util"
@@ -13,12 +14,19 @@ type Listener struct {
 	Logger util.Logger
 }
 
+func sudoIfNeeded(arg ...string) *exec.Cmd {
+	if os.Getuid() != 0 {
+		return exec.Command("sudo", arg...)
+	}
+	return exec.Command(arg[0], arg[1:]...)
+}
+
 func (h *Listener) doShutdown() error {
-	return exec.Command("shutdown", "-P", "1").Run()
+	return sudoIfNeeded("shutdown", "-P", "1").Run()
 }
 
 func (h *Listener) doShutdownAbort() error {
-	return exec.Command("shutdown", "-c").Run()
+	return sudoIfNeeded("shutdown", "-c").Run()
 }
 
 func (h *Listener) onReady(*http.Server) {
