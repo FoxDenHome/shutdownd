ad4abc2a8e4c6957be22100dafc43372c2b27198
Doridian <git@doridian.net>
2025-04-22 12:54:16 -0700

    sudoers

--- listener/http_other.go
+++ listener/http_other.go
@@ -16,17 +16,17 @@ type Listener struct {
 
 func sudoIfNeeded(arg ...string) *exec.Cmd {
 	if os.Getuid() != 0 {
-		return exec.Command("sudo", arg...)
+		return exec.Command("/usr/bin/sudo", arg...)
 	}
 	return exec.Command(arg[0], arg[1:]...)
 }
 
 func (h *Listener) doShutdown() error {
-	return sudoIfNeeded("shutdown", "-P", "1").Run()
+	return sudoIfNeeded("/usr/bin/shutdown", "-P", "1").Run()
 }
 
 func (h *Listener) doShutdownAbort() error {
-	return sudoIfNeeded("shutdown", "-c").Run()
+	return sudoIfNeeded("/usr/bin/shutdown", "-c").Run()
 }
 
 func (h *Listener) onReady(*http.Server) {
