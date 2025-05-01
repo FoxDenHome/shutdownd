a5dd1adb781d5e094b13f3cb03e63c52ef3970ea
Doridian <git@doridian.net>
2025-04-30 17:22:35 -0700

    simplify

--- build.sh
+++ build.sh
@@ -4,14 +4,9 @@ set -x
 
 export COMMIT="$(git rev-parse HEAD)"
 
-buildbin() {
-    go build \
-        -ldflags "-s -w -X github.com/FoxDenHome/shutdownd/util.commit=${COMMIT}" \
-        -trimpath \
-        "./cmd/$1"
-}
-
 rm -fv shutdownd certgen
 
-buildbin shutdownd
-buildbin certgen
+go build -o . \
+    -ldflags "-s -w -X github.com/FoxDenHome/shutdownd/util.commit=${COMMIT}" \
+    -trimpath \
+    "./cmd/..."
