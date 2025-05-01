b05341e7e74a5219c8a47434923cd2046fa5dd0e
Doridian <git@doridian.net>
2025-04-30 17:23:17 -0700

    up

--- build.sh
+++ build.sh
@@ -4,8 +4,6 @@ set -x
 
 export COMMIT="$(git rev-parse HEAD)"
 
-rm -fv shutdownd certgen
-
 go build -o . \
     -ldflags "-s -w -X github.com/FoxDenHome/shutdownd/util.commit=${COMMIT}" \
     -trimpath \
--- update.sh
+++ update.sh
@@ -4,5 +4,8 @@ set -x
 
 cd "$(realpath $(dirname "${0}"))"
 git pull
+git checkout main
+git reset --hard origin/main
+git clean -fdx
 ./build.sh
 sudo ./install.sh
